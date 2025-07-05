fae3159bc9f61fed27025f01ef54d39f6d6d2811
emjeisomDev <emjeisom.dev@gmail.com>
2025-07-05 15:15:08 -0300

    Main class created

---
+++ src/main/Main.java
@@ -0,0 +1,17 @@
+//-Transforme o trecho abaixo em forma de documentação em Java
+//-Faça uma documentação no idioma original.
+//-Faça uma documentaçã em português pt-br. 
+//-Caso o idioma original seja pt-br faça apenas em pt-br.
+//-NÃO FAÇA NENHUM CÓDIGO. APENAS A DOCUMENTAÇÃO.
+//-NÃO INTERPRETE O QUE DEVE SER FEITO. APENAS TRANSFORME O TEXTO NO FORMATO DE DOCUMENTAÇÃO DE CÓDIGO
+//-Dê um nome intuitivo para a classe
+// ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+// ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+//Otimize o código C# abaixo utilizando o mínimo de linhas de código possível
+//Dê preferência ao stream() e funções LAMBDA
+
+package main;
+
+public class Main {
+
+}
